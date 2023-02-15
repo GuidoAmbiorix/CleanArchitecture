96e40f7cd212d93ccc00af8cbf380a59c612700b
Guido Amparo <GuidoA_Amparo@jce.do>
2023-02-15 08:05:07 -0400

    Agregar archivos de proyecto.

---
+++ CleanArchitecture.Data/StreamerDbContext.cs
@@ -0,0 +1,37 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArchitecture.Data
+{
+    public class StreamerDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=LDEVELOPER28\MSSQLSERVER03;Initial Catalog=Streamer;Integrated Security=True;TrustServerCertificate=True")
+                .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information)
+                .EnableSensitiveDataLogging();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Streamer>()
+             .HasMany(m => m.Videos)
+             .WithOne(m => m.streamer)
+             .HasForeignKey(m => m.StreamerId)
+             .IsRequired()
+             .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Video>()
+                .HasMany(p => p.Actores)
+                .WithMany(t => t.Videos)
+                .UsingEntity<VideoActor>(
+                pt => pt.HasKey(e => new { e.ActorId, e.VideoId })
+                );
+        }
+
+        public DbSet<Streamer> Streamers { get; set; }
+        public DbSet<Video> Videos { get; set; }
+
+    }
+}
